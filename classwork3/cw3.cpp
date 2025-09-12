5a955b5f064fc0e5c2d9f29bc461b30118327e49
zuboda48 <zuboda48@29-3>
2025-09-12 11:52:18 +0300

    Add project files.

---
+++ classwork3/cw3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+	char *name;
+	int age;
+public:
+	Student(const char *n, int a)
+	{
+		name = new char[strlen(n) + 1];
+		strcpy_s(name, strlen(n) + 1, n);
+		age = a;
+	}
+	void Print()
+	{
+		cout << "Name: " << name << "\tAge: " << age << endl;
+	}
+	~Student()
+	{
+		cout << "Destruction\n";
+		delete[] name;
+	}
+};
+
+int main()
+{
+	Student obj1("Oleg", 19);
+	obj1.Print();
+}
