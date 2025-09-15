128a9c7d25a66595f2bc689eaa7b759a40b67b11
zuboda48 <zuboda48@29-3>
2025-09-15 11:26:09 +0300

    vfef

--- classwork3/cw3.cpp
+++ classwork3/cw3.cpp
@@ -3,28 +3,32 @@ using namespace std;
 
 class Student
 {
-	char *name;
+	char* name;
+	char* surname;
 	int age;
 public:
-	Student(const char *n, int a)
+	Student(const char *n, const char *s, int a)
 	{
 		name = new char[strlen(n) + 1];
 		strcpy_s(name, strlen(n) + 1, n);
+		surname = new char[strlen(s) + 1];
+		strcpy_s(surname, strlen(s) + 1, s);
 		age = a;
 	}
 	void Print()
 	{
-		cout << "Name: " << name << "\tAge: " << age << endl;
+		cout << "Name: " << name << "\tsurname: " << surname << "\tAge: " << age << endl;
 	}
 	~Student()
 	{
 		cout << "Destruction\n";
+		delete[] surname;
 		delete[] name;
 	}
 };
 
 int main()
 {
-	Student obj1("Oleg", 19);
+	Student obj1("Oleg", "Petrovich", 19);
 	obj1.Print();
 }
